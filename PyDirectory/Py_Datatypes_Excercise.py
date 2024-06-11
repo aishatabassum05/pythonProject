55a9fd8e753e1e3693b1ad8f4fd63ff72737a285
LENOVO <aishatabassum05@gmail.com>
2024-06-11 14:28:42 +0530

    Type conversion

---
+++ PyDirectory/Py_Datatypes_Excercise.py
@@ -0,0 +1,13 @@
+num1 = 50
+num2 = 40
+result = num1+num2
+print(result)
+num1=input("Enter a number")
+num2=input("enter a second number")
+result=num1+num2
+# result will have cancatenated value i.e num1+num2 like a string
+print(result)
+#to convert from str ot int, do type conversion
+result=int(num1)+int(num2)
+#now the result will be addition of 2 numbers recieved as input
+print(result)
